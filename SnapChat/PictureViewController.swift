421a63e4e78cf056be74f19dcd66d9b002ae5d80
Colleen Ng <colleenkyng@gmail.com>
2017-09-19 22:47:11 +0800

    Able to upload scaled down images unto Firebase

--- SnapChat/PictureViewController.swift
+++ SnapChat/PictureViewController.swift
@@ -7,23 +7,67 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
-class PictureViewController: UIViewController {
+class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var snapImageView: UIImageView!
     @IBOutlet weak var txtDescription: UITextField!
     @IBOutlet weak var nextButton: UIButton!
     
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        snapImageView.image = image
+        
+        snapImageView.backgroundColor = UIColor.clear
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
     @IBAction func btnCamera(_ sender: Any) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        present(imagePicker, animated: true, completion: nil)
+        imagePicker.allowsEditing = false
     }
     
     @IBAction func btnNext(_ sender: Any) {
+        nextButton.isEnabled = false
+        let imagesFolder = Storage.storage().reference().child("images")
+        
+        let imageData = UIImageJPEGRepresentation(snapImageView.image!, 0.1)!
+        
+        imagesFolder.child("\(NSUUID().uuidString).jpg").putData(imageData, metadata: nil, completion: {(metadata, error) in
+            print("We tried to upload!")
+            if error != nil {
+                print("We had an error: \(String(describing: error))")
+            } else {
+                print(metadata?.downloadURL() as Any)
+                
+                self.performSegue(withIdentifier: "selectUserSegue", sender: nil)
+                self.nextButton.isEnabled = true
+            }
+        })
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
